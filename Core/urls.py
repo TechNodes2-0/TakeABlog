b9110ae9397c30ec0c1dbfe83b0ad260d9c237d3
Vinayak Vispute <93467074+VinayakVispute@users.noreply.github.com>
2023-01-08 10:44:17 +0530

    Add files via upload

--- Core/urls.py
+++ Core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 # from . import views
 from .views import HomeView, ArticleDetail, AddPostView, UpdatePostView, CategoryView,LikeView
+from . import views
 
 urlpatterns = [
     # path('', views.homepage, name ="homepage"),
@@ -10,6 +11,7 @@ urlpatterns = [
     path('add_post/', AddPostView.as_view(), name="add_post"),
     path('article/edit_post/<int:pk>', UpdatePostView.as_view(), name="edit_post"),
     path('category/<str:cats>/', CategoryView, name="category"),
-    path('like/<int:pk>', LikeView, name='like_post')
+    path('like/<int:pk>', LikeView, name='like_post'),
+    path('chatbot/', views.chatbot, name='chatbot'),
 
 ]
